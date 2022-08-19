5bf137de57bd96327b9f76fda18e8c12fe50bad2
Kuni <kuni.ujiie@grapecity.com>
2022-08-19 11:38:16 +0900

    プロジェクト ファイルを追加します。

---
+++ WebApplication1-SupportApi/Controllers/SupportApiController.cs.cs
@@ -0,0 +1,12 @@
+﻿using GrapeCity.Documents.Pdf.ViewerSupportApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1_SupportApi.Controllers
+{
+    [Route("api/pdf-viewer")]
+    [ApiController]
+    public class SupportApiController : GcPdfViewerController
+    {
+
+    }
+}
